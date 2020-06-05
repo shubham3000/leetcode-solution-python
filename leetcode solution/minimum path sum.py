53d2b6b8ba4a131b0f7643c9fa9bc8ff9b2b4c35
shubham3000 <59827559+shubham3000@users.noreply.github.com>
2020-06-05 20:02:23 +0530

    Update minimum path sum.py

--- leetcode solution/minimum path sum.py
+++ leetcode solution/minimum path sum.py
@@ -1,22 +1,3 @@
-Minimum Path Sum
-
-Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
-
-Note: You can only move either down or right at any point in time.
-
-Input:
-[
-  [1,3,1],
-  [1,5,1],
-  [4,2,1]
-]
-
-Output: 7
-
-Explanation: Because the path 1--->3--->1--->1--->1 minimizes the sum.
-
-python solution
-
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         if len(grid)== 0 or len(grid[0])==0:
